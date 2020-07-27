080532fcdc01d7bc89902ad6301218a7432e2a05
Holly Prothe <hollyprothe1@gmail.com>
2020-07-26 20:05:56 -0500

    redo code again, start over

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Table from "./components/Table";
-import './App.css';
+import React from "react";
+import Directory from "./components/directory.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Table />
-      <Footer />
-    </div>
-  );
+    <Router>
+      <div>
+        <Route exact path="/" component={Directory} />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
---
+++ employeedirectory/src/components/search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Searchbar(props) {
+    return (
+        <div className="searchbar">
+            <form onSubmit={props.ignoreSubmit} className="form-inline">
+                <input
+                    value={props.search}
+                    onChange={props.handleInputChange}
+                    className="form-control"
+                    type="text"
+                    placeholder="Filter by name"
+                />
+            </form >
+        </div>
+    )
+}
+
+export default Searchbar;
