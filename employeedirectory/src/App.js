46af52ca4c774831471f2262e00c5c10c773fb42
Holly Prothe <hollyprothe1@gmail.com>
2020-07-26 19:31:38 -0500

    rewrite app js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import Main from './components/Main';
-import Wrapper from './components/Wrapper';
-import Header from './components/Header';
-import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Table from "./components/Table";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Wrapper>
-        <Header />
-        <Main />
-      </Wrapper>
+    <div>
+      <Header />
+      <Table />
+      <Footer />
     </div>
   );
 }
